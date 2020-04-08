cc976f30f5ab9a1bce4c0de4e44f024bbc6a381e
legyllith <jeremy.n.49@live.fr>
2020-04-08 16:34:44 +0200

    petite IA mise en place

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -18,6 +18,24 @@ public class Enemy : MonoBehaviour
         Vector3 dir = target.position - transform.position; //permet de creer le vecteur vers lequ'elle il doit aller
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
         //applique le déplacement, le normalize * speed permet d aller a la vitesse spped et * time.deltatime dans le temps
-    
+
+        float error = Mathf.Min(0.5f, 0.1f + 0.05f * speed);
+
+        if(Vector3.Distance(transform.position, target.position) <= error)// permet de prendre une compte les erreur de déplacement liée a unité
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint() // fonction passant au prochain waypoint
+    {  
+        if(waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
     }
 }
